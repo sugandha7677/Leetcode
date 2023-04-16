2b369986f197ef38b9bd7bf6c92f88287be7a480
sugandha tiwary <98490267+sugandha7677@users.noreply.github.com>
2023-04-16 12:53:57 +0530

    Time: 6 ms (43.56%), Space: 65.1 MB (14.48%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
@@ -1,29 +1,34 @@
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
-        int []ans = new int[nums.length];
         
-        int k = 0;
-        int pc = 0;
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i] < pivot){
-                ans[k++] = nums[i];
-                nums[i] = (int)1e9;
-            }
-            else if(nums[i] == pivot) {
-                pc++;
-                nums[i] = (int)1e9; 
-            }
-        }
-        
-        while(pc-- > 0){
-            ans[k++] = pivot;
-        }
+        int small = 0;
+        int same = 0;
         
         for(int i =0; i < nums.length; i++){
-            if(nums[i] != 1e9){
-                ans[k++] = nums[i];
-            }
+            
+            if(nums[i] < pivot) ++small;
+            
+            else if(nums[i] == pivot) ++same;
         }
-        return ans;
+        
+        int big = small + same;
+        int [] res = new int[nums.length];
+        same = small;
+        small = 0;
+        
+        for(int i =0; i < nums.length; i++){
+            
+            if(nums[i] < pivot){
+                res[small++] = nums[i];
+            }
+            else if(nums[i] == pivot){
+                res[same++] = pivot;
+            }
+            else res[big++] = nums[i];
+        }
+        
+        return res;
     }
+    
+    
 }
