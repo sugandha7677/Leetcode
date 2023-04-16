bf89454120ff7002c4a2409f435d4b7f973229d0
sugandha tiwary <98490267+sugandha7677@users.noreply.github.com>
2023-04-16 12:56:13 +0530

    Update 2161-partition-array-according-to-given-pivot.java

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
@@ -1,4 +1,39 @@
 class Solution {
+    
+    //A1. Brute
+    public int[] pivotArray(int[] nums, int pivot) {
+        int []ans = new int[nums.length];
+        
+        int k = 0;
+        int pc = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] < pivot){
+                ans[k++] = nums[i];
+                nums[i] = (int)1e9;
+            }
+            else if(nums[i] == pivot) {
+                pc++;
+                nums[i] = (int)1e9; 
+            }
+        }
+        
+        while(pc-- > 0){
+            ans[k++] = pivot;
+        }
+        
+        for(int i =0; i < nums.length; i++){
+            if(nums[i] != 1e9){
+                ans[k++] = nums[i];
+            }
+        }
+        return ans;
+    }
+    
+    // TC: O(n) + O(n)
+    // SC: O(1)
+    
+    //A2.
+    
     public int[] pivotArray(int[] nums, int pivot) {
         
         int small = 0;
@@ -30,5 +65,6 @@ class Solution {
         return res;
     }
     
-    
+    //TC: O(N)
+    //SC: O(N)
 }
