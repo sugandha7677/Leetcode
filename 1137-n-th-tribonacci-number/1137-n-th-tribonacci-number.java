1a1d71ba3e4519ba0ca2829e86a6cda09e8bee22
sugandha tiwary <98490267+sugandha7677@users.noreply.github.com>
2023-05-17 21:32:15 +0530

    Time: 0 ms (100.00%), Space: 39.4 MB (60.26%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -1,20 +1,19 @@
 class Solution {
     public int tribonacci(int n) {
         
-        int [] dp = new int [n+1];
-        Arrays.fill(dp, Integer.MIN_VALUE);
-        
-        return f(n, dp);
-    }
-    
-    int f(int n, int [] dp){
         if(n == 0 || n == 1) return n;
-        
         if(n == 2) return 1;
         
-        if(dp[n] != Integer.MIN_VALUE) return dp[n];
+        int [] dp = new int [n+1];
+        dp[0] = 0;
+        dp[1] = dp[2] = 1;
         
-        dp[n] = f(n-1 , dp) + f(n-2, dp) + f(n-3, dp);
+        for(int i = 3; i <= n; i++){
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+        }
+        
         return dp[n];
     }
+    
+    
 }
