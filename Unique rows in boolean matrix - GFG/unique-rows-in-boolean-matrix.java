93879f10898edbfa12cf7f7d649ba22ea2ca4927
sugandha tiwary <98490267+sugandha7677@users.noreply.github.com>
2023-06-25 10:28:19 +0530

    Added solution - LeetHub

---
+++ Unique rows in boolean matrix - GFG/unique-rows-in-boolean-matrix.java
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+import java.lang.Math;
+import java.io.*;
+import java.util.*;
+class UniqueRows{
+public static void main(String[] args)throws IOException
+ {  
+     BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+     PrintWriter out=new PrintWriter(System.out);
+		int t=Integer.parseInt(in.readLine().trim());
+		while(t-->0){
+		    String s[]=in.readLine().trim().split(" ");
+			int r=Integer.parseInt(s[0]);
+			int c=Integer.parseInt(s[1]);
+			s=in.readLine().trim().split(" ");
+			int ind=0;
+			int[][] a=new int [r][c];
+			for(int i=0;i<r;i++)
+				for(int j=0;j<c;j++)
+					a[i][j]=Integer.parseInt(s[ind++]);
+			GfG g=new GfG();
+			ArrayList<ArrayList<Integer>> arr = g.uniqueRow(a,r,c);
+			for(int i = 0;i<arr.size();i++){
+			    ArrayList<Integer> demo = arr.get(i);
+			    for(int j = 0;j<demo.size();j++){
+			        out.print(demo.get(j)+" ");
+			    }
+			    out.print("$");
+			}
+			out.println();
+		}
+		out.close();
+ }
+}
+// } Driver Code Ends
+
+
+/*Complete the given function*/
+class GfG
+{
+    public static ArrayList<ArrayList<Integer>> uniqueRow(int a[][],int r, int c)
+    {
+        //add code here.
+        ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+        
+        HashSet<String> set = new HashSet<>();
+        
+        for(int [] row : a){
+            
+           ArrayList<Integer> list = new ArrayList<>();
+           for(int i = 0; i < row.length; i++){
+               list.add(row[i]);
+           }
+           boolean isPresent  = false;
+           for(int i = 0;i < ans.size(); i++){
+               if(ans.get(i).equals(list)){
+                   isPresent = true;
+               }
+           }
+           
+           if(isPresent == false) ans.add(list);
+           
+        }
+        
+      
+        return ans;
+    }
+}
