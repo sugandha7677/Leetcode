0a486f568facd7bc8a19b69815868d51ca44cdb1
sugandha tiwary <98490267+sugandha7677@users.noreply.github.com>
2023-08-07 01:03:26 +0530

    Added solution - LeetHub

---
+++ Change the string - GFG/change-the-string.java
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class GfG
+{
+    public static void main (String[] args)
+    {
+        
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            String s = sc.next ();
+            Solution ob = new Solution();
+            String ans = ob.modify(s);
+            System.out.println(ans);
+        }
+        
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    String modify(String s){
+        char ch = s.charAt(0);
+        
+        if(Character.isLowerCase(ch)) return s.toLowerCase();
+        else return s.toUpperCase();
+        
+    }
+}
