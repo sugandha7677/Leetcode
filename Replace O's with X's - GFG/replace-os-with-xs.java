fd3c5c8d1e8080e68207e6c797ff7b25052509fb
sugandha tiwary <98490267+sugandha7677@users.noreply.github.com>
2023-09-04 20:16:11 +0530

    Added solution - LeetHub

---
+++ Replace O's with X's - GFG/replace-os-with-xs.java
@@ -0,0 +1,83 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while(t-- > 0){
+            String a[] = in.readLine().trim().split(" ");
+            int n = Integer.parseInt(a[0]);
+            int m = Integer.parseInt(a[1]);
+            char mat[][] = new char[n][m];
+            for(int i=0; i<n; i++)
+            {
+                String S[] = in.readLine().trim().split(" ");
+                for(int j=0; j<m; j++)
+                {
+                    mat[i][j] = S[j].charAt(0);
+                }
+            }
+            
+            Solution ob = new Solution();
+            char[][] ans = ob.fill(n, m, mat);
+            for(int i = 0;i < n;i++) {
+                for(int j = 0;j < m;j++) {
+                    System.out.print(ans[i][j] + " ");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    static char[][] fill(int n, int m, char a[][])
+    {
+        // code here
+    
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if ((i == 0 || i == n - 1 || j == 0
+                     || j == m - 1)
+                    && a[i][j] == 'O')
+                    dfs(a, n, m, i, j);
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (a[i][j] == 'O')
+                    a[i][j] = 'X';
+                else if (a[i][j] == '$')
+                    a[i][j] = 'O';
+            }
+        }
+        return a;
+      
+      
+     
+    }
+    static void dfs(char a[][], int n, int m, int i, int j)
+    {
+        if (i < 0 || i >= n || j < 0 || j >= m
+            || a[i][j] == 'X')
+            return;
+ 
+        if (a[i][j] == 'O') {
+            a[i][j] = '$';
+            dfs(a, n, m, i + 1, j);
+            dfs(a, n, m, i - 1, j);
+            dfs(a, n, m, i, j - 1);
+            dfs(a, n, m, i, j + 1);
+        }
+    }
+}
