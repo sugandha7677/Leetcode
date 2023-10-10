452d1f949ed7a40248688532ed824347e73f6c88
sugandha tiwary <98490267+sugandha7677@users.noreply.github.com>
2023-10-10 19:44:27 +0530

    Added solution - LeetHub

---
+++ Reverse words in a given string - GFG/reverse-words-in-a-given-string.java
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t > 0) {
+            String s = sc.next();
+            Solution obj = new Solution();
+            System.out.println(obj.reverseWords(s));
+            t--;
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Solution 
+{
+    //Function to reverse words in a given string.
+    String reverseWords(String S)
+    {
+        // code here 
+        String [] arr = S.split("\\.");
+        
+        
+        
+        String reverse = "";
+        
+        for(int i = arr.length - 1; i >= 0; i--){
+           
+            reverse = reverse + arr[i];
+            
+            if(i != 0)
+                reverse = reverse +  ".";
+        }
+        
+        return reverse;
+    }
+}
